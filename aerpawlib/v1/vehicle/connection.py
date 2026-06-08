e9aa93254cb41ca0fd438ab13ba60c66a37c89e8
jjreder <jjreder@gmail.com>
2026-06-07 23:21:42 -0400

    refactor: update imports

--- aerpawlib/v1/vehicle/connection.py
+++ aerpawlib/v1/vehicle/connection.py
@@ -16,8 +16,10 @@ Notes:
 from __future__ import annotations
 
 from aerpawlib._internal.connection_string import (
+    parse_udp_connection_port as _parse_udp_connection_port,  # noqa: F401
+)
+from aerpawlib._internal.connection_string import (
     validate_connection_scheme,
-    parse_udp_connection_port as _parse_udp_connection_port,
 )
 from aerpawlib.v1.exceptions import AerpawConnectionError
 
